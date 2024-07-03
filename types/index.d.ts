bd28fe269b0cd0926d0e42c6b4b1b9273e1780f1
Sensen xu <ss.xu@siriustech.io>
2024-07-03 17:16:29 +0800

    update

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+export default class HelloWorld {
+    /** Display value */
+    value: string | number
+
+    /** Maximum value, shows '{max}+' when exceeded. Only works if `value` is a number */
+    max: number
+
+    /** If a little dot is displayed */
+    isDot: boolean
+
+    /** Hidden badge */
+    hidden: boolean
+}
