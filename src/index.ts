7640b700aa39997a540c3b95ea1a75df1b2c6492
Sensen xu <ss.xu@siriustech.io>
2024-07-03 17:56:20 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { VueConstructor } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-    HelloWorld,
+
     install: (Vue: VueConstructor, opts = {}) => {
         Vue.component('HelloWorld', HelloWorld)
     }
