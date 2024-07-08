f90e74f8ddf0a1310d1e1bc2f56c518d8d9d1d3c
Sensen xu <ss.xu@siriustech.io>
2024-07-08 12:02:59 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { VueConstructor } from "vue";
-import { Opts } from "./types/index";
+import { Opts } from "../types/index";
 import Button from "./components/button/button.vue";
 import "./style/index.scss";
 
